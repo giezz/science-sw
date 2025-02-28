d47b2bacaf8622729ffe7b22349b76394f2a8de0
giezz <giezz@vk.com>
2025-02-28 16:27:24 +0300

    Лабы, работаем.

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,14 +1,26 @@
 import pandas as pd
 from dash import Dash, html
+import plotly.express as px
 
 df = pd.read_csv('../data/airline_data.csv')
 
 app = Dash()
 
+avg_weather = df.groupby(['Month', 'Reporting_Airline'])['WeatherDelay'].mean().reset_index()
+weather_figure = px.line(avg_weather, x='Month', y='WeatherDelay',
+                         color='Reporting_Airline',
+                         title='Average weather delay time (minutes) by airline')
+
 app.layout = [
     html.Header(html.Center(children='Flight Delay Time Statistics')),
-    html
+    html.Div(
+        children=[
+            html.Div(),
+            html.Div(),
+            html.Div()
+        ]
+    )
 ]
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
